0a1b046273bccbc21781dd6e52ca2892a2d9cf4d
Yoav Gal <yoav0gal@gmail.com>
2023-12-23 19:52:19 +0200

    solved stream

--- 02- Stream/stream-OG.py
+++ 02- Stream/stream-OG.py
@@ -1,50 +1,71 @@
 import threading
-import queue
+import time
 
 class Stream:
+    def wait(self):
+        while self.shouldRun:
+            if self.list:
+                for x in self.list:
+                    # Apply function
+                    if self.applyFunction is not None:
+                        self.handleApply(x)
+                    
+                    # Foreach function
+                    elif self.forEachFunction is not None:
+                        self.forEachFunction(x)
+                self.list = []
+            else: 
+                time.sleep(0.2)
+
+
+
     def __init__(self):
         self.shouldRun = True
         self.applyFunction = None
         self.forEachFunction = None
         self.newStream = None
-        self.data_queue = queue.Queue()
+        self.list = []
         self.thread = threading.Thread(target=self.wait)
         self.thread.start()
 
-    def wait(self):
-        while self.shouldRun or not self.data_queue.empty():
-            try:
-                x = self.data_queue.get(timeout=0.1)  # Timeout to check shouldRun periodically
-                self.process_item(x)
-            except queue.Empty:
-                continue
-
-    def process_item(self, x):
-        if self.applyFunction is not None:
-            self.handleApply(x)
-        elif self.forEachFunction is not None:
-            self.forEachFunction(x)
 
     def handleApply(self, item):
         applyFunctionResult = self.applyFunction(item)
+
+        # Filter apply function
         if applyFunctionResult and type(applyFunctionResult) is bool:
             self.newStream.add(item)
+        # Map apply function
         elif type(applyFunctionResult) is not bool:
             self.newStream.add(applyFunctionResult)
 
     def apply(self, applyFunction):
         self.applyFunction = applyFunction
         self.newStream = Stream()
+
+        if self.list:
+            for item in self.list:
+                self.handleApply(item)
+
+            self.list = []
+
         return self.newStream
 
     def forEach(self, forEachFunction):
         self.forEachFunction = forEachFunction
 
+        if self.list:
+            for item in self.list:
+                self.forEachFunction(item)
+
+            self.list = []
+
     def add(self, x):
-        self.data_queue.put(x)
+        self.list.append(x)
+
 
     def stop(self):
         self.shouldRun = False
-        self.thread.join()
         if self.newStream:
             self.newStream.stop()
+        self.thread.join()
